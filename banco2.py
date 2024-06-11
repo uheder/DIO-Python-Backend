05a52c9569a948c68811904011b4c81fe5ec4bed
Uheder Macedo <dev.usmacedo@outlook.com>
2024-06-11 16:29:58 -0300

    feat: implementadas funcoes criar usuario e conta, lista contas

--- banco2.py
+++ banco2.py
@@ -12,14 +12,38 @@ saques = []
 
 cont_saque_dia = 0
 usuarios = []
+contas = []
 
-def criar_usuario(nome, data_nasc, cpf, end):
-    pass
+def checa_cpf(cpf: int, usuarios: list):
+    for user in usuarios:
+        if user['cpf'] == cpf:
+            return True
+        return False
+    
+def criar_usuario(nome, data_nasc, cpf, endereco):
+    usuarios.append({'cpf': cpf, 'nome': nome, 'data_nasc': data_nasc, 'endereço': endereco})
+    print('Usuário criado com sucesso!')
+    return usuarios
 
-def criar_conta(agencia, conta, usuario):
-    pass
-
-def depositar(valor, saldo, extrato, /):
+def criar_conta(agencia, num_conta, cpf, contas):
+    for conta in contas:
+        if conta['cpf'] == cpf:
+            conta['contas'].append({'agencia': agencia, 'conta': num_conta})
+            print('Conta criada com sucesso!')
+            return contas
+    contas.append({'cpf': cpf, 'contas': [{'agencia': agencia, 'conta': num_conta}]})
+    print('Conta criada com sucesso!')
+    return contas
+def listar_contas(contas):
+    for conta in contas:
+        cpf = conta['cpf']
+        for lista in conta['contas']:
+            print(f"""\n
+            CPF titular: {cpf}\n
+            Agência: {lista['agencia']}\n
+            Nº Conta: {lista['conta']}""")
+    
+def depositar(saldo, valor, extrato, /):
     if valor <= 0:
         return "Valor de deposito inválido."
     saldo += valor
@@ -27,7 +51,7 @@ def depositar(valor, saldo, extrato, /):
     print("Depósito bem sucedido.")
     return saldo, extrato
     
-def sacar(valor, cont_saque_dia):
+def sacar(*, valor, cont_saque_dia):
     if saldo - valor < 0:
         return "Saldo insuficiente."
     elif valor > 500:
@@ -45,7 +69,7 @@ def sacar(valor, cont_saque_dia):
     print("Saque bem sucedido.")
     return saldo, extrato
 
-def gerar_extrato(saldo, extrato):
+def gerar_extrato(saldo, /, *, extrato):
     if extrato:
         print(f"{extrato} Seu saldo atual é: R$ {saldo: .2f}")
     else:
@@ -63,25 +87,47 @@ def menu():
         
         elif option.upper() == 'D':
             valor = float(input("Quanto deseja depositar? "))
-            depositar(valor, saldo, extrato) 
+            saldo, extrato = depositar(valor, saldo, extrato) 
             sleep(1)
 
         elif option.upper() == 'S':
             saque = float(input("Informe o valor a ser sacado: "))
-            sacar(saque, cont_saque_dia)
+            saldo, extrato = sacar(saque=saque, cont_saque_dia=cont_saque_dia)
             sleep(1)
             
         elif option.upper() == 'E':
-            gerar_extrato(saldo, extrato)
+            gerar_extrato(saldo,  extrato=extrato)
             sleep(1)
-        
-        elif option.upper() == 'C':
-            criar_conta()
-            sleep(1)
-        
+
         elif option.upper() == 'U':
-            criar_usuario()
+            cpf = int(input('Informe o numero do CPF do usuário: '))
+            if checa_cpf(cpf, usuarios):
+                print('CPF já cadastrado na lista de usuários')
+                sleep(1)
+                continue
+            nome = input('Informe o nome do usuário: ')
+            dt_nasc = input('Informe a data de nascimento no formato DD/MM/AAAA: ')
+            endereco = input('Informe seu endereço com logradouro - nº - bairro - cidade - sigla do estado: ')
+            criar_usuario(nome=nome, data_nasc=dt_nasc, cpf=cpf, endereco=endereco )
             sleep(1)
+
+                
+        elif option.upper() == 'C':
+            cpf = int(input('Informe o CPF para o cadastro da conta: '))
+            if not checa_cpf(cpf, usuarios):
+                print('Usuario não possui cadastro, impossivel criar conta.')
+                continue
+            criar_conta(agencia=AGENCIA, num_conta=len(contas) + 1, cpf=cpf, contas=contas)
+            sleep(1)
+        
+        
+        elif option.upper() == 'L':
+            if len(contas) == 0:
+                print('Não existem contas cadastradas')
+                sleep(1)
+                continue
+            listar_contas(contas)
+            sleep(2)
 
         else:
             print("Operação não reconhecida.")
