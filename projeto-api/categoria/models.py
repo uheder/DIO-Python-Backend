9a14e14b7fbff44af1d4619fa20667d7f09eb5ae
Uheder Macedo <dev.usmacedo@outlook.com>
2024-06-26 11:47:31 -0300

    fix: add constraints em cpf, nome de categoria e nome do ct

--- projeto-api/categoria/models.py
+++ projeto-api/categoria/models.py
@@ -7,6 +7,6 @@ class CategoriaModel(BaseModel):
     __tablename__ = 'categorias'
 
     pk_id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    nome: Mapped[str] = mapped_column(String(10), nullable=False)
+    nome: Mapped[str] = mapped_column(String(10), unique=True, nullable=False)
     atleta: Mapped['AtletaModel'] = relationship(back_populates='categoria')
     atleta_id = Mapped[int] = mapped_column(ForeignKey('atletas.pk_id'))
