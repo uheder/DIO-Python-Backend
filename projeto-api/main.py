bae26a348c296ded9fa8bc78f780fa9b54ddae28
Uheder Macedo <dev.usmacedo@outlook.com>
2024-06-26 12:17:22 -0300

    fix: atualizada porta

--- projeto-api/main.py
+++ projeto-api/main.py
@@ -5,4 +5,4 @@ import uvicorn
 app = FastAPI(title='WorkoutAPI')
 
 if __name__ == '__main__':
-    uvicorn.run('main:app', host='127.0.0.1', port=8080, log_level='info', reload=True)
+    uvicorn.run('main:app', host='127.0.0.1', port=5432, log_level='info', reload=True)
